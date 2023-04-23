4a7d9389ea2d838a702fca54bb4af93dff498bac
4lon <alon.nusem@gmail.com>
2023-04-23 14:22:14 +1000

    Refactored motor code for easier usage in logic.

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -17,6 +17,8 @@ int moisture;
 int pump = 2;
 int waterThreshold = 360;
 
+int lightThreshold = 100;
+
 // Define Motor Driver pins
 int motorAEnablePin = 5;
 int motorBEnablePin = 6;
@@ -35,8 +37,63 @@ void initMotors() {
   // pinMode(motorBInput2, OUTPUT);
 
   // Enable both motors
+//  digitalWrite(motorAEnablePin, LOW);
+//  digitalWrite(motorBEnablePin, LOW);
   digitalWrite(motorAEnablePin, LOW);
   digitalWrite(motorBEnablePin, LOW);
+}
+
+void rightMotorForward() {
+  digitalWrite(motorBEnablePin, HIGH);
+  digitalWrite(motorBInput1, LOW);
+}
+
+void rightMotorBackward() {
+  digitalWrite(motorBEnablePin, HIGH);
+  digitalWrite(motorBInput1, HIGH);
+}
+
+void rightMotorOff() {
+  digitalWrite(motorBEnablePin, LOW);
+}
+
+void leftMotorForward() {
+  digitalWrite(motorAEnablePin, HIGH);
+  digitalWrite(motorAInput1, HIGH);
+}
+
+void leftMotorBackward() {
+  digitalWrite(motorAEnablePin, HIGH);
+  digitalWrite(motorAInput1, LOW);
+}
+
+void leftMotorOff() {
+  digitalWrite(motorAEnablePin, LOW);
+}
+
+void turnLeft() {
+  rightMotorForward();
+  leftMotorBackward();
+}
+
+void turnRight() {
+  rightMotorBackward();
+  leftMotorForward();
+}
+
+void driveForward() {
+  rightMotorForward();
+  leftMotorForward();
+}
+
+void driveBackward() {
+  rightMotorBackward();
+  leftMotorBackward();
+}
+
+void stop() {
+  rightMotorOff();
+  leftMotorOff();
 }
 
 /**
@@ -45,25 +102,47 @@ void initMotors() {
  */
 void motorTest() {
   while (true) {
-    // Make Motor A spin forward
-    digitalWrite(motorAInput1, HIGH);
-    // digitalWrite(motorAInput2, LOW);
-
-    // Make Motor B spin backward
-    digitalWrite(motorBInput1, HIGH);
-    // digitalWrite(motorBInput2, HIGH);
-
-    // Wait for 2 seconds
-    delay(2000);
-
-    // Stop both motors
-    digitalWrite(motorAInput1, LOW);
-    // digitalWrite(motorAInput2, LOW);
-    digitalWrite(motorBInput1, LOW);
-    // digitalWrite(motorBInput2, LOW);
-
-    // Wait for 1 second
+    rightMotorForward();
     delay(1000);
+    rightMotorBackward();
+    delay(1000);
+    rightMotorOff();
+    delay(500);
+    leftMotorForward();
+    delay(1000);
+    leftMotorBackward();
+    delay(1000);
+    leftMotorOff();
+    delay(500);
+    rightMotorForward();
+    leftMotorForward();
+    delay(1000);
+    rightMotorBackward();
+    leftMotorBackward();
+    delay(1000);
+    rightMotorOff();
+    leftMotorOff();
+    delay(5000);
+    
+//    // Make Motor A spin forward
+//    digitalWrite(motorAInput1, HIGH);
+//    // digitalWrite(motorAInput2, LOW);
+//
+//    // Make Motor B spin backward
+//    digitalWrite(motorBInput1, HIGH);
+//    // digitalWrite(motorBInput2, HIGH);
+//
+//    // Wait for 2 seconds
+//    delay(2000);
+//
+//    // Stop both motors
+//    digitalWrite(motorAInput1, LOW);
+//    // digitalWrite(motorAInput2, LOW);
+//    digitalWrite(motorBInput1, LOW);
+//    // digitalWrite(motorBInput2, LOW);
+//
+//    // Wait for 1 second
+//    delay(1000);
   }
 }
 
@@ -119,7 +198,6 @@ void waterPlant() {
   long time = millis();
   digitalWrite(pump, HIGH);
   while ((analogRead(moisturePin) > waterThreshold) && (millis() < time + 1000)) {
-    count++;
   }
   digitalWrite(pump, LOW);
 }
@@ -128,12 +206,22 @@ void turnToSun() {
   takeReading();
   while ((max(flLight, blLight) != flLight) && (max(frLight, brLight) != frLight)) {
 //    Not facing max light
-  if (max(flLight, blLight) > (max(frLight, brLight)) {
+  if (max(flLight, blLight) > (max(frLight, brLight))) {
 //    turn left
   } else {
 //    turn right
   }
-    takeReading()
+    takeReading();
+  }
+}
+
+void drive() {
+  takeReading();
+
+  while (((flLight + frLight)/2) < lightThreshold) {
+    // drive forward
+    delay(100);
+    takeReading();
   }
 }
 
